3adc63272434a2199a075722aa95ca0aac47359c
Aamirgheewale <aamirgheewale142210@.com>
2025-09-11 09:35:12 +0530

    +page: added active wishlist page

---
+++ src/routes/wishlist/+page.svelte
@@ -0,0 +1,23 @@
+
+<script lang="ts">
+
+  import { wishlist, removeFromWishlist } from '$lib/wishlistStore';
+  import type { WishlistItem } from '$lib/wishlistStore';
+  let books : WishlistItem[] = [];
+  wishlist.subscribe(value => books = value);
+</script>
+
+<h2>Wishlist ({books.length})</h2>
+<div class="wishlist-list">
+  {#each books as book}
+    <div class="wishlist-item">
+      <img src={book.image} alt={book.title} />
+      <div>{book.title}</div>
+      <div>{book.price}</div>
+      <button on:click={() => removeFromWishlist(book.id)}>🗑️</button>
+    </div>
+  {/each}
+  {#if books.length === 0}
+    <div>No books added to wishlist.</div>
+  {/if}
+</div>

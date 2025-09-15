9e694cd71d9558e72493b91db28603359b3c185b
Aamirgheewale <aamirgheewale142210@.com>
2025-09-15 09:44:24 +0530

    types: updated optional export interface

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,19 @@
 export interface Book {
-  id: string | number;
+  id?: string | number;
+  title?: string;
+  author?: string;
+  price?: number;
+  img?: string;
+  loggedIn?: boolean;
+  firstName?: string;
+  lastName?: string;
+  cover_image_url?: string;
+ 
+}
+export interface WishlistItem {
+  id: string;
   title: string;
   author: string;
   price: number;
-  img: string;
+  image?: string;
 }

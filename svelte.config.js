c0e49a4e4105e6c8987ab661472628e1e49adb01
Aamirgheewale <aamirgheewale142210@.com>
2025-09-18 19:57:19 +0530

    added vitepreprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';  // Corrected import
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
 
